05c9fb7befb535e247358fcd16cdea14882e7035
Nuno <35053@ufp.edu.pt>
2019-01-03 02:23:04 +0000

    nothing

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -1,30 +1,31 @@
 @extends('layout')
 
 @section('content')
+<div class="row justify-content-center">
 <h1>Pedidos</h1>
+</div>
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-3">
 
-            <form method="post" action="/orders">
+            <form method="post" action="/orders" id="add_name">
                 {{csrf_field()}}
-                <div class="form-group">
+                <div class="form-group" id="dynamic_field">
                     <label for="exampleInputEmail1">Quantidade</label>
                     <input type="text" class="form-control is-valid" id="exampleInputEmail1"
                            aria-describedby="emailHelp"
-                           name="quantity" placeholder="Numero" value="{{old('quantity')}}" required >
-                    </small>
+                           name="name[]" placeholder="Numero" value="{{old('quantity')}}" required >
                 </div>
         </div>
 
         <div class="col-6">
             <div class="form-group">
                 <label for="exampleInputPassword1">Produto</label>
-                <input type="text" class="form-control is-valid" id="exampleInputPassword1" name="description" placeholder="Texto"  value="{{ old('description')}}" required>
+                <input type="text" class="form-control is-valid" id="exampleInputPassword1" name="description[]" placeholder="Texto"  value="{{ old('description')}}" required>
             </div>
         </div>
     </div>
-
+    <button type="button" name="add" id="add" class="btn btn-success">Add More</button></td>
     <div class="row">
         <div class="col">
             <button type="submit" class="btn btn-primary">Submit</button>
@@ -35,6 +36,82 @@
 </div>
 
 </form>
+
+
+
+
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        var postURL = "<?php echo url('addmore'); ?>";
+        var i=1;
+
+
+        $('#add').click(function(){
+            i++;
+
+            $('#dynamic_field').append('<tr id="row'+i+'" class="dynamic-added">' +
+                '<th scope="row">1</th>'+
+                '<td>' +
+                '<input type="text" class="form-control is-valid" id="exampleInputEmail aria-describedby="emailHelp" name="name[]" placeholder="Numero" value="{{old('quantity')}}" required > ' +
+            '<input type="text" class="form-control is-valid" id="coluna'+i+'" name="description[]" placeholder="Texto"  value="{{ old('description')}}" required>  ' +
+            '</td>' +
+            '<td>' +
+            '<button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button>' +
+            '</td>' +
+            '</tr>');
+
+
+        });
+
+
+        $(document).on('click', '.btn_remove', function(){
+            var button_id = $(this).attr("id");
+            $('#row'+button_id+'').remove();
+        });
+
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+
+        $('#submit').click(function(){
+            $.ajax({
+                url:postURL,
+                method:"POST",
+                data:$('#add_name').serialize(),
+                type:'json',
+                success:function(data)
+                {
+                    if(data.error){
+                        printErrorMsg(data.error);
+                    }else{
+                        i=1;
+                        $('.dynamic-added').remove();
+                        $('#add_name')[0].reset();
+                        $(".print-success-msg").find("ul").html('');
+                        $(".print-success-msg").css('display','block');
+                        $(".print-error-msg").css('display','none');
+                        $(".print-success-msg").find("ul").append('<li>Record Inserted Successfully.</li>');
+                    }
+                }
+            });
+        });
+
+
+        function printErrorMsg (msg) {
+            $(".print-error-msg").find("ul").html('');
+            $(".print-error-msg").css('display','block');
+            $(".print-success-msg").css('display','none');
+            $.each( msg, function( key, value ) {
+                $(".print-error-msg").find("ul").append('<li>'+value+'</li>');
+            });
+        }
+    });
+</script>
 
 @endsection
 <!--
