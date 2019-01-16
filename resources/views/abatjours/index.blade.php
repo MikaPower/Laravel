fdf7467135ed5c414e0c978c93a96e5bae1270a5
mikapower <35053@ufp.edu.pt>
2019-01-16 21:06:59 +0000

    navbar left

--- resources/views/abatjours/index.blade.php
+++ resources/views/abatjours/index.blade.php
@@ -1,28 +1,21 @@
 @extends('layout')
-<div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-left">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
 
+
+<div class="container-fluid">
+<nav class="navbar-primary">
+    <a href="#" class="btn-expand-collapse"><span class="glyphicon glyphicon-menu-left"></span></a>
+    <ul class="navbar-primary-menu">
+        <li>
+            <a href="#"><span class="glyphicon glyphicon-list-alt"></span><span class="nav-label">Dashboard</span></a>
+            <a href="#"><span class="glyphicon glyphicon-envelope"></span><span class="nav-label">Profile</span></a>
+            <a href="#"><span class="glyphicon glyphicon-cog"></span><span class="nav-label">Settings</span></a>
+            <a href="#"><span class="glyphicon glyphicon-film"></span><span class="nav-label">Notification</span></a>
+            <a href="#"><span class="glyphicon glyphicon-calendar"></span><span class="nav-label">Monitor</span></a>
+        </li>
+    </ul>
+</nav>
+</div>
+<div class="container col-3">
 <h1>Abatjours Apresentacao</h1>
 
 @foreach ($abatjours as $abatjour)
@@ -31,3 +24,10 @@
 </div>
 </body>
 </html>
+
+
+<script>
+    $('.btn-expand-collapse').click(function(e) {
+        $('.navbar-primary').toggleClass('collapsed');
+    });
+</script>
