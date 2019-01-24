1db58252ab4ef258dd537b3963a732c6fbc3beb0
mikapower <35053@ufp.edu.pt>
2019-01-24 20:12:04 +0000

    index

--- resources/views/abatjours/index.blade.php
+++ resources/views/abatjours/index.blade.php
@@ -1,112 +1,53 @@
-<!DOCTYPE html>
-<html>
+@extends('layout')
+@section('content')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<div class="container">
+    @if($abatjours->count())
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
 
+        <?php
+        $count=0;
 
-    <title>Collapsible sidebar using Bootstrap 4</title>
-
-    <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
-          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <!-- Our Custom CSS -->
-
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-
-
-    <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
-            integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
-            crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
-            integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
-            crossorigin="anonymous"></script>
-</head>
-
-<body>
-
-
-<div class="wrapper">
-    <!-- Sidebar -->
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <h3>J.M Pinto Abat-Jours</h3>
-        </div>
-
-        <ul class="list-unstyled components">
-            <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false"
-                   class="dropdown-toggle">Abatjours</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                    <li>
-                        <a href="#">Home 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Home 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Home 3</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#pedidoSubmenu" data-toggle="collapse" aria-expanded="false"
-                   class="dropdown-toggle">Pedidos</a>
-                <ul class="collapse list-unstyled" id="pedidoSubmenu">
-                    <li>
-                        <a href="#">Page 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 3</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Sobre Nós</a>
-            </li>
-            <li>
-                <a href="#">Contactos</a>
-            </li>
-        </ul>
-    </nav>
-
-    <div id="content">
-        <div class="container-fluid">
-            <button type="button" class="btn btn-default btn-lg" id="sidebarCollapse" style="background-color: #000000;color: white;">Menu
-                <i class="fas fa-align-justify"></i>
-            </button>
-        </div>
+        foreach ($abatjours as $abatjour) {
+            if($count==0){                               //caso seja inicio da row
+                echo "<tr>";
+                echo "   <th scope=\"row\">1</th>";
+            }
+            if($count==2) {
+                ?>
+                 <td>  <img src="/storage/images/{{$abatjour->imagemodels->filename}}"/> </td>";
+        <?php
+                $count=0;
+              echo " </tr>";            //se for o ultima da row, fecho a row e reinicio count;
+            }
+            else{
+                echo "   <td><img src=\"/storage/images/$abatjour->imagemodels->filename\"
+                 style=\"max-width: 100%;max-height:100%; height: auto;\"/></td>";
+                $count++;
+            }
+        }
+        ?>
+        </tbody>
+    </table>
+@endif
 </div>
 
 
-<!-- jQuery CDN - Slim version (=without AJAX) -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<!-- Popper.JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
-        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
-        crossorigin="anonymous"></script>
-<!-- Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
-        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
-        crossorigin="anonymous"></script>
-<script>
-    $(document).ready(function () {
 
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-        });
 
-    });
-</script>
 
-</body>
 
-</html>
+
+
+
+
+@endsection
