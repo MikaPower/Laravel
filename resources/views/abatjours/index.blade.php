489e74598aa09987f1c6c9fc7daef94385bd17de
Nuno <35053@ufp.edu.pt>
2019-01-25 00:19:03 +0000

    show almost done

--- resources/views/abatjours/index.blade.php
+++ resources/views/abatjours/index.blade.php
@@ -3,10 +3,9 @@
 
 <div class="container">
     @if($abatjours->count())
-    <table class="table">
+    <table class="table" style="table-layout: fixed">
         <thead>
         <tr>
-            <th scope="col">#</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
             <th scope="col">Handle</th>
@@ -20,20 +19,38 @@
         foreach ($abatjours as $abatjour) {
             if($count==0){                               //caso seja inicio da row
                 echo "<tr>";
-                echo "   <th scope=\"row\">1</th>";
             }
             if($count==2) {
                 ?>
-                 <td>  <img src="/storage/images/{{$abatjour->imagemodels->filename}}"
-                            style="max-width: 100%;max-height:100%; height: auto;" alt="{{ $abatjour->imagemodels->filename  }}"/> </td>
+                 <td>
+                     <div class="card style" style="max-width: 100%;">
+                         <img class="card-img-top" src="/storage/images/{{$abatjour->imagemodels->filename}}" alt="Card image cap"
+                              style="max-width: 100%;max-height:100%; height: auto;">
+                         <div class="card-body">
+                             <h5 class="card-title">Card title</h5>
+                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                             <a href="#" class="btn btn-primary">Go somewhere</a>
+                         </div>
+                     </div>
+
+
+
+
+
+
+
+
+
+
+                      </td>
         <?php
                 $count=0;
               echo " </tr>";            //se for o ultima da row, fecho a row e reinicio count
             }
             else{
                 ?>
-          <td><img src="/storage/images/{{$abatjour->imagemodels->filename}}"
-                   style="max-width: 100%;max-height:100%; height: auto;" alt="{{ $abatjour->imagemodels->filename  }}"/></td>
+          <td><img src="/storage/images/{{$abatjour->imagemodels->filename}}" class="img-fluid" alt="Responsive image"
+                   style="max-width: 100%;max-height:100%; height: auto;"/></td>
         <?php
                 $count++;
             }
