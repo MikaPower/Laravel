2fdb482f3f83b8bb00c44ef8a5d129f86c14315b
mikapower <35053@ufp.edu.pt>
2019-01-26 20:35:30 +0000

    trying to commit

--- app/Http/Controllers/AbatjoursController.php
+++ app/Http/Controllers/AbatjoursController.php
@@ -18,7 +18,7 @@ class AbatJoursController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auths');
     }
 
     public function index()
